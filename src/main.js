68ceede1b240a25d8a0899c4d97f52c66edcd0ee
lml6666 <1483557675@qq.com>
2019-07-29 20:06:47 +0800

    登录表单验证

--- src/main.js
+++ src/main.js
@@ -6,9 +6,17 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible/index.min.js'
-
+import VeeValidate, { Validator } from 'vee-validate'
+// 中文
+import ZH_CN from 'vee-validate/dist/locale/zh_CN'
+Validator.extend('mobile', {
+  getMessage: field => '请输入正确的手机号',
+  validate: value =>
+    value.length === 11 && /^((13|14|15|17|18|19)[0-9]{1}\d{8})$/.test(value)
+})
+Vue.use(VeeValidate)
 Vue.use(Vant)
-
+Validator.localize('ZH_CN', ZH_CN)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,16 @@
   <div class="login">
     <van-nav-bar title="登录" />
     <!-- 用户名和密码 输入框 -->
+    <!-- <input v-validate="'required|email'" type="text" name="email">
+    <span v-show="errors.has('email')">{{ errors.first('email') }}</span> -->
     <form action="/">
       <van-cell-group>
-        <van-field v-model="user.mobile" label="手机号" placeholder="请输入手机号" />
+        <!-- <van-field v-model="user.mobile" label="手机号" placeholder="请输入手机号" /> -->
+        <van-field v-validate="'required|mobile'" v-model="user.mobile" name="phone" :error-message="errors.first('phone')" label="手机号" placeholder="请输入手机号" />
 
         <van-field v-model="user.code" label="验证码" placeholder="请输入验证码" type="password" />
       </van-cell-group>
-      <van-button type="info" block @click.prevent="handelLogin">登录</van-button>
+      <van-button type="info" :loading="loadingLogin"  block @click.prevent="handelLogin">登录</van-button>
     </form>
   </div>
 </template>
@@ -23,19 +26,35 @@ export default {
       user: {
         mobile: '18801185985',
         code: '246810'
-      }
+      },
+      loadingLogin: false
     }
   },
   methods: {
     async handelLogin () {
-      try {
-        const data = await login(this.user)
-        console.log(data)
-        // 更新token
-        this.$store.commit('setUsertoken', data)
-      } catch (error) {
-        console.log(error)
-      }
+      // 开启加载效果
+      this.loadingLogin = true
+      // 1.如果验证失败就 return
+      this.$validator.validate().then(async valid => {
+        if (!valid) {
+          // 关闭加载效果
+          this.loadingLogin = false
+          console.log('验证失败了')
+          return
+        }
+        // 2.验证成功才能进入下面的程序
+        try {
+          const data = await login(this.user)
+          console.log(data)
+          // 登录时，重新获取token，更新token
+          this.$store.commit('setUsertoken', data)
+          // 登录成功，跳转到首页
+          this.$router.push({ path: '/' })
+        } catch (error) {
+          this.loadingLogin = false
+          console.log(error)
+        }
+      })
     }
   }
 }
